085a7463021177d4cb21d58934af44af3a68948a
Iagohss <iago.silva@ccc.ufcg.edu.br>
2024-07-31 19:59:08 -0300

    Criando testes para os ingressos

---
+++ SistemaIngressos/src/tests/TarefaTest.java
@@ -0,0 +1,71 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TarefaTest {
+
+	@Test
+	void testCriarIngressoValido() {
+		try {
+			double  precoBase = 10;
+			Ingresso ingresso = new Ingresso(precoBase, TipoIngresso.NORMAL);
+		}catch(IllegalArgumentException e){
+			assertEquals("Entrada inválida", e.getMessage());
+		}
+	}
+	
+	@Test
+	void testGetPrecoIngressoNORMAL() {
+		double  precoBase = 10;
+		double  precoEsperado = 10;
+		Ingresso ingresso = new Ingresso(precoBase, Status.NORMAL);
+		assertEquals(precoEsperado, ingresso.getPreco());
+	}
+
+	@Test
+	void testGetPrecoIngressoVIP() {
+		double  precoBase = 10;
+		double  precoEsperado = 15;
+		Ingresso ingresso = new Ingresso(precoBase, Status.VIP);
+		assertEquals(precoEsperado, ingresso.getPreco());
+	}
+	
+	@Test
+	void testGetPrecoIngressoMEIA_ENTRADA() {
+		double  precoBase = 10;
+		double  precoEsperado = 5;
+		Ingresso ingresso = new Ingresso(precoBase, Status.MEIA_ENTRADA);
+		assertEquals(precoEsperado, ingresso.getPreco());
+	}
+	
+	@Test
+	void testIngressoInicializaNAO_VENDIDO() {
+		Status statusEsperado = Status.NAO_VENDIDO;
+		Ingresso ingresso = new Ingresso(10, Status.NORMAL);
+		
+		assertEquals(statusEsperado, ingresso.getStatus());
+	}
+	
+	@Test
+	void testVendaDeIngresso() {
+		Status statusEsperado = Status.VENDIDO;
+		Ingresso ingresso = new Ingresso(10, Status.NORMAL);
+		ingresso.setStatus(Status.VENDIDO);
+		
+		assertEquals(statusEsperado, ingresso.getStatus());
+	}
+	
+	@Test
+	void testVendeIngressoDuasVezes() {
+		try {
+			Status statusEsperado = Status.VENDIDO;
+			Ingresso ingresso = new Ingresso(10, Status.NORMAL);
+			ingresso.setStatus(Status.VENDIDO);
+			ingresso.setStatus(Status.VENDIDO);
+		}catch(IllegalArgumentException e){
+			assertEquals("Ingresso já vendido", e.getMessage());
+		}
+	}
+}
